48de9d2b657f633ca9e2aff3939f8c86e3b437ae
上村晃平 <cope73123@gmail.com>
2019-12-14 14:28:45 +0900

    プロジェクト ファイルを追加します。

---
+++ LaserHarpHost/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Path1_0 = New System.Windows.Forms.TextBox()
+        Me.Path1_1 = New System.Windows.Forms.TextBox()
+        Me.Path1_2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Path2_2 = New System.Windows.Forms.TextBox()
+        Me.Path2_1 = New System.Windows.Forms.TextBox()
+        Me.Path2_0 = New System.Windows.Forms.TextBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Path3_2 = New System.Windows.Forms.TextBox()
+        Me.Path3_1 = New System.Windows.Forms.TextBox()
+        Me.Path3_0 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Path4_2 = New System.Windows.Forms.TextBox()
+        Me.Path4_1 = New System.Windows.Forms.TextBox()
+        Me.Path4_0 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.KeyIn = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Path1_0
+        '
+        Me.Path1_0.Location = New System.Drawing.Point(3, 3)
+        Me.Path1_0.Name = "Path1_0"
+        Me.Path1_0.ReadOnly = True
+        Me.Path1_0.Size = New System.Drawing.Size(165, 25)
+        Me.Path1_0.TabIndex = 0
+        '
+        'Path1_1
+        '
+        Me.Path1_1.Location = New System.Drawing.Point(3, 34)
+        Me.Path1_1.Name = "Path1_1"
+        Me.Path1_1.ReadOnly = True
+        Me.Path1_1.Size = New System.Drawing.Size(165, 25)
+        Me.Path1_1.TabIndex = 1
+        '
+        'Path1_2
+        '
+        Me.Path1_2.Location = New System.Drawing.Point(3, 65)
+        Me.Path1_2.Name = "Path1_2"
+        Me.Path1_2.ReadOnly = True
+        Me.Path1_2.Size = New System.Drawing.Size(165, 25)
+        Me.Path1_2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Button1.Location = New System.Drawing.Point(32, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 43)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Path1_2)
+        Me.Panel1.Controls.Add(Me.Path1_1)
+        Me.Panel1.Controls.Add(Me.Path1_0)
+        Me.Panel1.Location = New System.Drawing.Point(32, 79)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(173, 95)
+        Me.Panel1.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Button2.Location = New System.Drawing.Point(218, 30)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(173, 43)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Button3.Location = New System.Drawing.Point(397, 30)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(173, 43)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Path2_2)
+        Me.Panel2.Controls.Add(Me.Path2_1)
+        Me.Panel2.Controls.Add(Me.Path2_0)
+        Me.Panel2.Location = New System.Drawing.Point(218, 79)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(173, 95)
+        Me.Panel2.TabIndex = 6
+        '
+        'Path2_2
+        '
+        Me.Path2_2.Location = New System.Drawing.Point(3, 65)
+        Me.Path2_2.Name = "Path2_2"
+        Me.Path2_2.ReadOnly = True
+        Me.Path2_2.Size = New System.Drawing.Size(165, 25)
+        Me.Path2_2.TabIndex = 2
+        '
+        'Path2_1
+        '
+        Me.Path2_1.Location = New System.Drawing.Point(3, 34)
+        Me.Path2_1.Name = "Path2_1"
+        Me.Path2_1.ReadOnly = True
+        Me.Path2_1.Size = New System.Drawing.Size(165, 25)
+        Me.Path2_1.TabIndex = 1
+        '
+        'Path2_0
+        '
+        Me.Path2_0.Location = New System.Drawing.Point(3, 3)
+        Me.Path2_0.Name = "Path2_0"
+        Me.Path2_0.ReadOnly = True
+        Me.Path2_0.Size = New System.Drawing.Size(165, 25)
+        Me.Path2_0.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Path3_2)
+        Me.Panel3.Controls.Add(Me.Path3_1)
+        Me.Panel3.Controls.Add(Me.Path3_0)
+        Me.Panel3.Location = New System.Drawing.Point(397, 79)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(173, 95)
+        Me.Panel3.TabIndex = 7
+        '
+        'Path3_2
+        '
+        Me.Path3_2.Location = New System.Drawing.Point(3, 65)
+        Me.Path3_2.Name = "Path3_2"
+        Me.Path3_2.ReadOnly = True
+        Me.Path3_2.Size = New System.Drawing.Size(165, 25)
+        Me.Path3_2.TabIndex = 2
+        '
+        'Path3_1
+        '
+        Me.Path3_1.Location = New System.Drawing.Point(3, 34)
+        Me.Path3_1.Name = "Path3_1"
+        Me.Path3_1.ReadOnly = True
+        Me.Path3_1.Size = New System.Drawing.Size(165, 25)
+        Me.Path3_1.TabIndex = 1
+        '
+        'Path3_0
+        '
+        Me.Path3_0.Location = New System.Drawing.Point(3, 3)
+        Me.Path3_0.Name = "Path3_0"
+        Me.Path3_0.ReadOnly = True
+        Me.Path3_0.Size = New System.Drawing.Size(165, 25)
+        Me.Path3_0.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Button4.Location = New System.Drawing.Point(576, 30)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(173, 43)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.Path4_2)
+        Me.Panel4.Controls.Add(Me.Path4_1)
+        Me.Panel4.Controls.Add(Me.Path4_0)
+        Me.Panel4.Location = New System.Drawing.Point(576, 79)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(173, 95)
+        Me.Panel4.TabIndex = 7
+        '
+        'Path4_2
+        '
+        Me.Path4_2.Location = New System.Drawing.Point(3, 65)
+        Me.Path4_2.Name = "Path4_2"
+        Me.Path4_2.ReadOnly = True
+        Me.Path4_2.Size = New System.Drawing.Size(165, 25)
+        Me.Path4_2.TabIndex = 2
+        '
+        'Path4_1
+        '
+        Me.Path4_1.Location = New System.Drawing.Point(3, 34)
+        Me.Path4_1.Name = "Path4_1"
+        Me.Path4_1.ReadOnly = True
+        Me.Path4_1.Size = New System.Drawing.Size(165, 25)
+        Me.Path4_1.TabIndex = 1
+        '
+        'Path4_0
+        '
+        Me.Path4_0.Location = New System.Drawing.Point(3, 3)
+        Me.Path4_0.Name = "Path4_0"
+        Me.Path4_0.ReadOnly = True
+        Me.Path4_0.Size = New System.Drawing.Size(165, 25)
+        Me.Path4_0.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(551, 309)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 18)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Label1"
+        '
+        'KeyIn
+        '
+        Me.KeyIn.Location = New System.Drawing.Point(559, 399)
+        Me.KeyIn.Name = "KeyIn"
+        Me.KeyIn.Size = New System.Drawing.Size(174, 44)
+        Me.KeyIn.TabIndex = 9
+        Me.KeyIn.Text = "Button5"
+        Me.KeyIn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 472)
+        Me.Controls.Add(Me.KeyIn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Path1_0 As TextBox
+    Friend WithEvents Path1_1 As TextBox
+    Friend WithEvents Path1_2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Path2_2 As TextBox
+    Friend WithEvents Path2_1 As TextBox
+    Friend WithEvents Path2_0 As TextBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Path3_2 As TextBox
+    Friend WithEvents Path3_1 As TextBox
+    Friend WithEvents Path3_0 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Path4_2 As TextBox
+    Friend WithEvents Path4_1 As TextBox
+    Friend WithEvents Path4_0 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents KeyIn As Button
+End Class
---
+++ LaserHarpHost/Form1.vb
@@ -0,0 +1,201 @@
+﻿Public Class Form1
+    Dim ofd As New OpenFileDialog()
+    Dim validMode As New Byte
+    Dim pathList(11, 7) As String
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        validMode = 1 'モードの初期化
+
+        '「ファイルを開く」ダイアログ初期設定
+
+        'はじめのファイル名を指定する
+        'はじめに「ファイル名」で表示される文字列を指定する
+        ofd.FileName = ""
+        'はじめに表示されるフォルダを指定する
+        '指定しない（空の文字列）の時は、現在のディレクトリが表示される
+        ofd.InitialDirectory = ""
+        '[ファイルの種類]に表示される選択肢を指定する
+        '指定しないとすべてのファイルが表示される
+        ofd.Filter = "waveファイル(*.wav;*.wave)|*.wav;*.wave|すべてのファイル(*.*)|*.*"
+        '[ファイルの種類]ではじめに選択されるものを指定する
+        '2番目の「すべてのファイル」が選択されているようにする
+        ofd.FilterIndex = 1
+        'タイトルを設定する
+        ofd.Title = "開くファイルを選択してください"
+        'ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+        ofd.RestoreDirectory = True
+        '存在しないファイルの名前が指定されたとき警告を表示する
+        'デフォルトでTrueなので指定する必要はない
+        ofd.CheckFileExists = True
+        '存在しないパスが指定されたとき警告を表示する
+        'デフォルトでTrueなので指定する必要はない
+        ofd.CheckPathExists = True
+
+    End Sub
+
+    Private Sub Path1_0_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path1_0.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(1, 0) = ofd.FileName
+
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path1_0.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path1_1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path1_1.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(1, 1) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path1_1.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path1_2_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path1_2.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(1, 2) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path1_2.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path2_0_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path2_0.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(2, 0) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path2_0.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path2_1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path2_1.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(2, 1) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path2_1.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path2_2_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path2_2.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(2, 2) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path2_2.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path3_0_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path3_0.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(3, 0) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path3_0.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path3_1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path3_1.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(3, 1) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path3_1.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path3_2_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path3_2.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(3, 2) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path3_2.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path4_0_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path4_0.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path4_0.Text = System.IO.Path.GetFileName(ofd.FileName)
+            pathList(4, 0) = ofd.FileName
+        End If
+    End Sub
+
+    Private Sub Path4_1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path4_1.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(4, 1) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path4_1.Text = System.IO.Path.GetFileName(ofd.FileName)
+        End If
+    End Sub
+
+    Private Sub Path4_2_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Path4_2.MouseDoubleClick
+        'ダイアログを表示する
+        If ofd.ShowDialog() = DialogResult.OK Then
+            pathList(4, 2) = ofd.FileName
+            'OKボタンがクリックされたとき、選択されたファイル名を表示する
+            Path4_2.Text = System.IO.Path.GetFileName(ofd.FileName)
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ModeChange(1)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ModeChange(2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ModeChange(3)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ModeChange(4)
+    End Sub
+
+    Private Sub KeyIn_KeyPress(sender As Object, e As KeyPressEventArgs) Handles KeyIn.KeyPress
+        Label1.Text = e.KeyChar
+        If IsNumeric(e.KeyChar) = True Then
+            Dim KeyByte As Byte
+            KeyByte = Convert.ToInt16(e.KeyChar)
+            ModeChange(KeyByte)
+            'If 0 < KeyByte < 5 Then
+            'ModeChange(KeyByte)
+            'End If
+        End If
+    End Sub
+
+    Private Function ModeChange(ByVal vMo As Byte)
+        'validModeの変更と、ボタン表示の変更
+        Dim cs As Control() = Me.Controls.Find("Button1", True)
+        validMode = vMo
+
+        'すべてのテキストボックスを初期色化
+        For i As Byte = 1 To 4
+            cs = Me.Controls.Find("Button" + i.ToString, True)
+            If cs.Length > 0 Then
+                CType(cs(0), Button).BackColor = SystemColors.ControlLight
+                CType(cs(0), Button).Font = New Font(CType(cs(0), Button).Font, FontStyle.Regular)
+                CType(cs(0), Button).ForeColor = SystemColors.ControlText
+            End If
+        Next i
+
+        '押されたボタンの背景色、文字色、太文字設定
+        cs = Me.Controls.Find("Button" + vMo.ToString, True)
+        If cs.Length > 0 Then
+            CType(cs(0), Button).BackColor = SystemColors.Highlight
+            CType(cs(0), Button).Font = New Font(CType(cs(0), Button).Font, FontStyle.Bold)
+            CType(cs(0), Button).ForeColor = SystemColors.Control
+        End If
+
+    End Function
+
+End Class
